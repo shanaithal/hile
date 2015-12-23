ca4586ddc12090d6ad706e899702214fa5176024
Shankara G <shanaithal@gmail.com>
2015-12-23 17:28:30 +0530

    [M] Adding utility changes for pagination

--- utilities/index.js
+++ utilities/index.js
@@ -1,3 +1,5 @@
+var config = require('../config');
+
 var Utility = function () {
 
 	var utilityObject = Object.create(Utility.prototype);
@@ -26,14 +28,52 @@ Utility.prototype.getFormattedResponse = function (resultSet) {
 
 Utility.prototype._getFilters = function (queryParams) {
 
-	var filters = [];
-	for (var queryParam in queryParams) {
-		if (queryParam) {
-			filters.push(queryParam);
-		}
-	}
+	var filters = queryParams;
+	delete filters.page;
+	delete filters.count;
 	//var clone = JSON.parse(JSON.stringify(queryParams));
 	return filters;
 }
 
+Utility.prototype.getNextPage = function (path, page, count) {
+
+	path = path.replace(/[p][a][g][e][=][0-9]+[&]/i, "").replace(/[p][a][g][e][=][0-9]+/i, "");
+	path = path.replace(/[c][o][u][n][t][=][0-9]+[&]/i, "").replace(/[c][o][u][n][t][=][0-9]+/i, "");
+	path = config.service_url + path;
+	if (path.indexOf('?') > -1) {
+		path = path + "page=" + page + "&count=" + count;
+	} else {
+		path = path + "&page=" + page + "&count=" + count;
+	}
+	
+	return _getLinkObject(path, "next");
+};
+
+Utility.prototype.getPreviousPage = function (path, page, count) {
+
+	path = path.replace(/[p][a][g][e][=][0-9]+[&]/i, "").replace(/[p][a][g][e][=][0-9]+/i, "");
+	path = path.replace(/[c][o][u][n][t][=][0-9]+[&]/i, "").replace(/[c][o][u][n][t][=][0-9]+/i, "");
+	path = config.service_url + path;
+	if (path.indexOf('?') > -1) {
+		path = path + "page=" + page + "&count=" + count;
+	} else {
+		path = path + "&page=" + page + "&count=" + count;
+	}
+
+	return _getLinkObject(path, "prev");
+};
+
+function _removePageCountQueryParams(path) {
+
+	return path;
+}
+
+function _getLinkObject(path, rel) {
+
+	var linkObject = {};
+	linkObject.href = path;
+	linkObject.rel = rel;
+
+	return linkObject;
+}
 module.exports = Utility;
